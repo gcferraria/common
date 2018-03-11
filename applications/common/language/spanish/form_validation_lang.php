1ba219381e31222fe629926bfbfb49b7b513ab0e
Gonçalo Ferraria <gferraria@gmail.com>
2018-03-11 20:08:22 +0000

    Add language for spanish

---
+++ applications/common/language/spanish/form_validation_lang.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang = array (
+      'required'           => 'Campo Obrigatório.'
+    , 'valid_email'        => 'Email Inválido.'
+    , 'valid_phone'        => 'Formato inv&aacute;lido'
+    , 'valid_nif'          => 'Formato inv&aacute;lido'
+    , 'valid_domain'       => 'Formato inv&aacute;lido'
+    , 'is_unique'          => 'Valor j&aacute; existente'
+    , 'spaces'             => 'O %s não pode conter espa&ccedil;os'
+    , 'uriname'            => 'O %s não tem um valor v&aacute;lido para URI'
+    , 'matches'            => 'O valor do campo não &eacute; coincidente'
+    , 'numeric'            => 'O valor não &eacute; num&eacute;rico'
+    , 'min_string_size'    => 'O campo %s deve ser no min&iacute;mo %s caracteres'
+    , 'max_string_size'    => 'O campo %s deve ser no m&aacute;ximo %s caracteres'
+    , 'unespected_error'   => 'Erro inesperado, Contacte o Administrador'
+    , 'unique_combination' => 'J&aacute; existe uma combina&ccedil;&atilde;o para este campo'
+);
