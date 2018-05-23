31abb8e035b0d6dd7f02b296923a5691fc347295
Gonçalo Ferraria <gferraria@gmail.com>
2018-05-23 19:55:47 +0100

    Added form validation messages in english

---
+++ applications/common/language/english/form_validation_lang.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang = array (
+      'required'           => 'Required Field.'
+    , 'valid_email'        => 'Invalid Email.'
+    , 'valid_phone'        => 'Invalid Format'
+    , 'valid_nif'          => 'Invalid Format'
+    , 'valid_domain'       => 'Invalid Format'
+    , 'is_unique'          => 'The value already exists.'
+);
