b4b9803b4ed7df45da015179c04d73348185d485
Gonçalo Ferraria <gferraria@gmail.com>
2018-11-07 22:15:29 +0000

    German Language

---
+++ applications/common/language/german/form_validation_lang.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang = array (
+      'required'           => 'Pflichtfeld.'
+    , 'valid_email'        => 'Ungültige E-Mail.'
+    , 'valid_phone'        => 'Ungültiges Format'
+    , 'valid_nif'          => 'Ungültiges Format'
+    , 'valid_domain'       => 'Ungültiges Format'
+    , 'is_unique'          => 'Der Wert ist bereits vorhanden.'
+);
