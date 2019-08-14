a0d1f157eb497a744aaacc8f3b8e724e604c9461
Gonçalo Ferraria <gferraria@gmail.com>
2019-08-14 22:42:19 +0100

    Fix javascript:void(0)

--- applications/common/libraries/Breadcrumb.php
+++ applications/common/libraries/Breadcrumb.php
@@ -233,7 +233,7 @@ class Breadcrumb
                         $link .= '</a>';
                     }
                     else
-                        $link  = '<a href="JavaScript:Void(0)">' . $crumb['text'] . '</a>';
+                        $link  = '<a href="javascript:void(0)">' . $crumb['text'] . '</a>';
 
                     $this->_output .= $wrapper_inline[0] . $link . $wrapper_inline[1];
                 }
